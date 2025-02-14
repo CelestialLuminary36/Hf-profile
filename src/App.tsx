8fb873a32d0fb6ca379a8a80a723ccb94b2424ea
hhy <1713471338@qq.com>
2025-02-14 18:36:18 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import { OrbitingCirclesDemo } from './components/demo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <OrbitingCirclesDemo />
+    </>
+  )
+}
+
+export default App
